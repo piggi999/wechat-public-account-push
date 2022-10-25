7f4067ce72b116cb455d4037949ff18103330ce3
piggi999 <87293723+piggi999@users.noreply.github.com>
2022-10-25 15:04:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRYxg6SKeimGVDYDiRABhPud6uxw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'xxDobZkjlEkMG12xEVYgJFYrrAFXNbSSGCW0IcBs5QY',
+      useTemplateId: 'FcWYw-wPlPtf-ezvHbM-jV4gEsUISIzeg54_HjjrbnA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-01',
       festivals: [
@@ -33,10 +33,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2002', date: '03-01',
+          type: '生日', name: 'piggi', year: '1999', date: '11-21',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '11-29',
+          type: '节日', name: '学期结束', year: '2023', date: '01-08',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
